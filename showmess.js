b998be6df92109d06b2288316751aa699690f78f
Ергенова Поліна Сергіївна <Erge_ns81>
2019-12-08 14:45:07 +0200

    New

---
+++ showmess.js
@@ -0,0 +1,54 @@
+function showMessage() {
+    alert( "всем привет");
+}
+
+showMessage();
+
+showMessage();
+
+showMessage();
+
+
+
+function sum() {
+    feturn a + b;
+}
+var s = sum();
+alert ("Sum: " + a);
+
+function div(firstNum, secondNum) {
+    alert (firstNum + " * " +
+    secondNum + " = " +
+    firstNum * secondNum));
+return firstNum * secondNum
+}
+
+
+
+function Start (count) {
+    for(var i = 0; i < count; i++) {
+        document.write(text" * ");
+    }
+    document.write("<br>");
+}
+Start(count: 30);
+Start(count: 10);
+Start(count: 50);
+
+
+
+function vivod(num) {
+    var n = parsent(num);
+    if (n > 0 || n < 10) alert(n);
+    else alert("Нет совпадений");
+}
+vivod(num:15);
+
+function AnyLine(symb, count) {
+    for (var i = 0; i < count; i++){
+        document.write(sumb);
+    }
+    document.write("<br>");
+}
+
+
